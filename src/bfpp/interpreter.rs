72350832c20938c033a79cd81aa572eebbb74b0f
Icosa <117495728+H4drian@users.noreply.github.com>
2024-05-13 10:08:46 -0400

    Update interpreter.rs

--- src/bfpp/interpreter.rs
+++ src/bfpp/interpreter.rs
@@ -1,3 +1,12 @@
+/*
+ * MIT License
+ * 
+ * Copyright (c) 2024 Leon Cotten
+ * 
+ * This language is provided under the MIT Licence.
+ * See LICENSE for more information.
+ */ 
+
 // bfpp built-in brainfuck interpreter
 
 pub fn interpret_str(code: &str) -> () { 
