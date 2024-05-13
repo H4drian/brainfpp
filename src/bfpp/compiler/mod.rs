69aa3393ae99c1f729a425621aeb3e39e5066098
Icosa <117495728+H4drian@users.noreply.github.com>
2024-05-13 10:08:58 -0400

    Update mod.rs

--- src/bfpp/compiler/mod.rs
+++ src/bfpp/compiler/mod.rs
@@ -1,3 +1,12 @@
+/*
+ * MIT License
+ * 
+ * Copyright (c) 2024 Leon Cotten
+ * 
+ * This language is provided under the MIT Licence.
+ * See LICENSE for more information.
+ */ 
+
 use crate::bfpp::lexer;
 use lexer::Token;
 use lexer::Lexem;
