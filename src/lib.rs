c01e782191ffe25be515f49f2616509433d0053d
Icosa <117495728+H4drian@users.noreply.github.com>
2024-05-29 16:11:59 -0400

    updated lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
+/*
+ * MIT License
+ * 
+ * Copyright (c) 2024 Leon Cotten
+ * 
+ * This language is provided under the MIT Licence.
+ * See LICENSE for more information.
+ */ 
+
 mod bfpp;
 pub use bfpp::{
     interpreter,
